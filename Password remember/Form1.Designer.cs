c94aa221afecf6c4b5844b5820528e29226ac9cb
remp <kirilrempov@gmail.com>
2018-01-30 18:10:59 +0300

    init

---
+++ Password remember/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Password_remember
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtField = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "new"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(111, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(173, 28);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(57, 21);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtField
+            // 
+            this.txtField.Location = new System.Drawing.Point(12, 96);
+            this.txtField.Multiline = true;
+            this.txtField.Name = "txtField";
+            this.txtField.Size = new System.Drawing.Size(218, 126);
+            this.txtField.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtField);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtField;
+    }
+}
+
---
+++ Password remember/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Password_remember
+{
+    public partial class Form1 : Form
+    {
+        public Dictionary<string, Dictionary<string, string>> data;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SerializeData(string path)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream fs = File.Open(path, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                formatter.Serialize(fs, data);
+            }
+        }
+
+        private void DeserializeData(string path)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (FileStream fs = File.Open(path, FileMode.Open, FileAccess.Read))
+            {
+                data = (Dictionary<string, Dictionary<string, string>>)formatter.Deserialize(fs);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
